c96fe71b20fa995e90512fb7f5c001a0e6fd7536
Ангелина Панькова <Tyuiu.PankovaAA@outlook.com>
2025-11-03 18:08:55 +0500

    Task 2

---
+++ Tyuiu.PankovaAA.Sprint4.Task2.V27.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PankovaAA.Sprint4.Task2.V27.Lib;
+namespace Tyuiu.PankovaAA.Sprint4.Task2.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+
+            DataService ds = new DataService();
+            int[] array = { 2, 4, 6, 3, 5, 2, 7, 4, 6, 2, 5, 4, 7 };
+            int res = ds.Calculate(array);
+            int wait = 2 + 4 + 6 + 2 + 4 + 6 + 2 + 4;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PankovaAA.Sprint4.Task2.V27/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.PankovaAA.Sprint4.Task2.V27.Lib;
+namespace Tyuiu.PankovaAA.Sprint4.Task2.V27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 |Выполнила: Панькова А.А.| ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  Спринт #4                                                              *");
+            Console.WriteLine("*  Тема: одномерные массивы(генератор случайных чисел)                    *");
+            Console.WriteLine("*  Задание #02                                                            *");
+            Console.WriteLine("*  Вариант #27                                                            *");
+            Console.WriteLine("*  Выполнила: Панькова Ангелина Алексеевна    |ПИНб-25-1|                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 13 элементов заполненный         *");
+            Console.WriteLine("* случайными в диапазоне от 2 до 7 подсчитать сумму четных элементов      *");
+            Console.WriteLine("* массива.                                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+
+            int[] array = { 2, 4, 6, 3, 5, 2, 7, 4, 6, 2, 5, 4, 7 };
+            int sum = ds.Calculate(array);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  РЕЗУЛЬТАТ:                                                             *");
+
+            Console.WriteLine("Четные элементы: 2 + 4 + 6 + 2 + 4 + 6 + 2 + 4 = " + sum);
+
+            Console.ReadKey();
+
+
+
+        }
+
+    }
+}
