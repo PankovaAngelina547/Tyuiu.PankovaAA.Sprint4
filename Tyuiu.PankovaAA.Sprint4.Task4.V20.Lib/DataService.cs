72025385160b2919acc24fd274c6115464f7827c
Ангелина Панькова <Tyuiu.PankovaAA@outlook.com>
2025-11-04 19:58:50 +0500

    Task 4

---
+++ Tyuiu.PankovaAA.Sprint4.Task4.V20.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.PankovaAA.Sprint4.Task4.V20.Lib
+{
+    public class DataService : ISprint4Task4V20
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+            int[,] result = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (matrix[i, j] % 2 == 0)
+                    {
+                        result[i, j] = 1; 
+                    }
+                    else
+                    {
+                        result[i, j] = matrix[i, j]; 
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.PankovaAA.Sprint4.Task4.V20/Program.cs
@@ -0,0 +1,64 @@
+﻿using Tyuiu.PankovaAA.Sprint4.Task4.V20.Lib;
+namespace Tyuiu.PankovaAA.Sprint4.Task4.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 |Выполнила: Панькова А.А.| ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  Спринт #4                                                              *");
+            Console.WriteLine("*  Тема: двумерные массивы (ввод с клавиатуры)                            *");
+            Console.WriteLine("*  Задание #04                                                            *");
+            Console.WriteLine("*  Вариант #20                                                            *");
+            Console.WriteLine("*  Выполнила: Панькова Ангелина Алексеевна    |ПИНб-25-1|                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 4 до 8. Заменить четные элементы *");
+            Console.WriteLine("* массива на 1                                                            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  РЕЗУЛЬТАТ:                                                             *");
+
+            Console.WriteLine("Введите массив 5x5 (значения от 4 до 8):");
+
+            int[,] matrix = new int[5, 5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write($"Элемент [{i},{j}]: ");
+                    int value = Convert.ToInt32(Console.ReadLine());
+
+
+                    if (value < 4 || value > 8)
+                    {
+                        Console.WriteLine("Ошибка! Значение должно быть от 4 до 8");
+                        j--;
+                        continue;
+                    }
+
+                    matrix[i, j] = value;
+
+
+                   
+
+
+
+                }
+
+            }
+        }
+    }
+}
