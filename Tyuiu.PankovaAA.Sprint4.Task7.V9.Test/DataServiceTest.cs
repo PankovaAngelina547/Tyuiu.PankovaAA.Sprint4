a2bfb9a1064c0a0bbb0e01c8d1290b9cccffe973
Ангелина Панькова <Tyuiu.PankovaAA@outlook.com>
2025-11-10 17:37:23 +0500

    Task 7

---
+++ Tyuiu.PankovaAA.Sprint4.Task7.V9.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.PankovaAA.Sprint4.Task7.V9.Lib;
+namespace Tyuiu.PankovaAA.Sprint4.Task7.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            int rows = 3;
+            int columns = 3;
+            string str = "864299753";
+            DataService ds = new DataService();
+
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 4; 
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PankovaAA.Sprint4.Task7.V9/Program.cs
@@ -0,0 +1,62 @@
+﻿using Tyuiu.PankovaAA.Sprint4.Task7.V9.Lib;
+namespace Tyuiu.PankovaAA.Sprint4.Task6.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int rows = 3;
+            int columns = 3;
+            string str = "864299753";
+            Console.Title = "Спринт #4 |Выполнила: Панькова А.А.| ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  Спринт #4                                                              *");
+            Console.WriteLine("*  Тема:                                                                  *");
+            Console.WriteLine("*  Задание #07                                                            *");
+            Console.WriteLine("*  Вариант #09                                                            *");
+            Console.WriteLine("*  Выполнила: Панькова Ангелина Алексеевна    |ПИНб-25-1|                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            int index = 0;
+            Console.WriteLine("\nМатрица:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+
+            
+            int result = ds.Calculate(rows, columns, str);
+
+            Console.WriteLine($"\nКоличество четных чисел: {result}");
+
+           
+            Console.WriteLine("\nАнализ четности:");
+            foreach (char c in str)
+            {
+                int num = int.Parse(c.ToString());
+                string parity = (num % 2 == 0) ? "четное" : "нечетное";
+                Console.WriteLine($"{num} - {parity}");
+            }
+
+            Console.ReadKey();
+        }
+
+        
+
+
+
+        
+    }
+}
